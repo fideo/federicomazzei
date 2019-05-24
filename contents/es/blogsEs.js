930c325d3a673ecec7d760f9c2de2a042761b257
Federico Mazzei <federicomazzei@gmail.com>
2019-05-23 23:19:40 -0300

    agrego una nota mas

--- contents/es/blogsEs.js
+++ contents/es/blogsEs.js
@@ -1,5 +1,6 @@
 export default [
   '20170915-crear-un-repositorio-git-en-servidor-propio',
   '20171121-mega-patin-day-2017',
-  '20170925-reaction-commerce-usando-propio-servidor'
+  '20170925-reaction-commerce-usando-propio-servidor',
+  '20180219-deploy-meteor-en-tu-propio-servidor'
 ]
