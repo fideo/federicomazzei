e6cd4ec1b839f00fcde9456a5a9e9b2e53ee2e0e
Federico Mazzei <federicomazzei@gmail.com>
2019-09-26 19:44:10 -0300

    agrego dos notas al blog y pongo text-truncate en el titulo y descripcion

--- contents/es/blogsEs.js
+++ contents/es/blogsEs.js
@@ -1,7 +1,9 @@
 export default [
-  '20170925-reaction-commerce-usando-propio-servidor',
-  '20180219-deploy-meteor-en-tu-propio-servidor',
-  '20170915-crear-un-repositorio-git-en-servidor-propio',
-  '20171121-mega-patin-day-2017',
-  '20170915-actualizar-localmente-wordpress-da-fallida'
+	'20190926-comprimir-descomprimir-por-consola-linux-con-7zip',
+	'20190926-mongodump-mongorestore',
+	'20170925-reaction-commerce-usando-propio-servidor',
+	'20180219-deploy-meteor-en-tu-propio-servidor',
+	'20170915-crear-un-repositorio-git-en-servidor-propio',
+	'20171121-mega-patin-day-2017',
+	'20170915-actualizar-localmente-wordpress-da-fallida'
 ]
