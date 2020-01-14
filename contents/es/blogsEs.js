631de95e24bf9e79e5a7193e61287246837c2fe3
fideo <federicomazzei@gmail.com>
2020-01-14 09:31:07 -0300

    publico la nota sobre grive

--- contents/es/blogsEs.js
+++ contents/es/blogsEs.js
@@ -1,10 +1,11 @@
 export default [
-	'20191001-algunos-comandos-utiles-de-linux',
-	'20190926-comprimir-descomprimir-por-consola-linux-con-7zip',
-	'20190926-mongodump-mongorestore',
-	'20170925-reaction-commerce-usando-propio-servidor',
-	'20180219-deploy-meteor-en-tu-propio-servidor',
-	'20170915-crear-un-repositorio-git-en-servidor-propio',
-	'20171121-mega-patin-day-2017',
-	'20170915-actualizar-localmente-wordpress-da-fallida'
+  '20200113-sincronizar-archivos-google-drive-linux',
+  '20191001-algunos-comandos-utiles-de-linux',
+  '20190926-comprimir-descomprimir-por-consola-linux-con-7zip',
+  '20190926-mongodump-mongorestore',
+  '20170925-reaction-commerce-usando-propio-servidor',
+  '20180219-deploy-meteor-en-tu-propio-servidor',
+  '20170915-crear-un-repositorio-git-en-servidor-propio',
+  '20171121-mega-patin-day-2017',
+  '20170915-actualizar-localmente-wordpress-da-fallida'
 ]
